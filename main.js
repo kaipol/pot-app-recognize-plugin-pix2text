254b31c4a6d707a98b2f15ebcfbc67082fb96832
kaipol <108178985+kaipol@users.noreply.github.com>
2024-11-23 03:22:45 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 async function recognize(base64, lang, options) {
     const {config, utils } = options;
-    const { run, cacheDir,pluginDir } = utils;
-    let {device}=config;
+    const { run, cacheDir, pluginDir } = utils;
+    let {device} = config;
     
     if (device == null) {
         device = "cpu"; 
@@ -20,11 +20,15 @@ async function recognize(base64, lang, options) {
         "--save-debug-res",
         `${cacheDir}/pot_screenshot_cut.png-result.jpg`,
     ]);
+    
     if (result.status === 0) {
         let out = result.stdout;
-        out = out.split("Outs:");
-        out = out[1];
-        return out.trim();
+        out = out.split("Outs:\n");
+        if (out.length > 1) {
+            return out[1].trim();
+        } else {
+            throw Error("Output does not contain 'Outs:\\n'");
+        }
     } else {
         throw Error(result.stderr);
     }
